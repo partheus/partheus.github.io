6a84885a467912e358b535377ba750f8a66293a9
Parth <parth.shiralkar@gmail.com>
2022-03-21 22:52:12 -0700

    Add CNAME file to build

--- .eleventy.js
+++ .eleventy.js
@@ -36,6 +36,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/favicons");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
 
   eleventyConfig.setBrowserSyncConfig({
