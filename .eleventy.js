c663f1c2a060c9055f52f95a7328f0fd716b1bc6
Parth <parth.shiralkar@gmail.com>
2022-03-22 23:24:34 -0700

    Add tag functionality to all entries page

--- .eleventy.js
+++ .eleventy.js
@@ -39,6 +39,22 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
 
 
+  function filterTagList(tags) {
+    return (tags || []).filter(tag => ["entry", "nav", "post", "posts"].indexOf(tag) === -1);
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList)
+
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(item => {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    });
+
+    return filterTagList([...tagSet]);
+  });
+
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, bs) {
